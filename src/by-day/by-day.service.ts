300fe5d763f739f5e6ff5dc9d45eb942fabe0907
Giedrius <kalniusg@gmail.com>
2022-01-19 18:05:59 +0200

    Added few changes in by day service.

--- src/by-day/by-day.service.ts
+++ src/by-day/by-day.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { map, Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { ByDay } from 'src/entity/byDay.entity';
 import { Repository } from 'typeorm';
 
@@ -12,36 +12,33 @@ export class ByDayService {
     private readonly httpService: HttpService,
   ) {}
 
-  findAll(date: string, country: string): Observable<string> {
+  // findAll(date: string, country: string): Observable<string> {
+  findAll(date: string, country: string): Subscription {
     return this.httpService
       .get(
-        ` https://kayaposoft.com/enrico/json/v2.0?action=isPublicHoliday&date=${date}&country=${country}`,
+        `https://kayaposoft.com/enrico/json/v2.0?action=isPublicHoliday&date=${date}&country=${country}`,
       )
-      .pipe(
-        map((response) => response.data),
-        map((data) => {
-          if (data.isPublicHoliday === false) {
-            this.httpService
-              .get(
-                ` https://kayaposoft.com/enrico/json/v2.0?action=isWorkDay&date=${date}&country=${country}`,
-              )
-              .pipe(
-                map((response) => response.data),
-                map((data) => {
-                  if (data.isWorkDay === false) {
-                    return 'free day';
-                  } else {
-                    const string = JSON.stringify(data);
-                    return string;
-                  }
-                }),
-              );
-          } else {
-            const string = JSON.stringify(data);
-            return string;
-          }
-        }),
-      );
+      .subscribe((value) => {
+        if (value.data.isPublicHoliday === true) {
+          console.log(value.data);
+          return JSON.stringify(value.data);
+        } else {
+          return this.httpService
+            .get(
+              `https://kayaposoft.com/enrico/json/v2.0?action=isWorkDay&date=${date}&country=${country}`,
+            )
+            .subscribe((value) => {
+              if (value.data.isWorkDay === true) {
+                console.log(value.data);
+                return JSON.stringify(value.data);
+              } else {
+                console.log('free day');
+                const string = JSON.stringify({ freeDay: 'free day' });
+                return string;
+              }
+            });
+        }
+      });
   }
 
   getAll(): Promise<ByDay[]> {
